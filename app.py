961a2712721f7c85e6de0808b5200bab5361c3c5
Danial Afzal <dafzal@lyft.com>
2018-04-13 19:57:36 -0700

    endpoint

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 #Python libraries that we need to import for our bot
 import random
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
 
 app = Flask(__name__)
 
 #We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/ulu", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def receive_message():
     print request.get_json()
-    return "YOOO LETS GOOOO2"
+    return jsonify(foo='bar')
+
+@app.route('/')
+def foo():
+    return 'hi'
 
 if __name__ == "__main__":
     app.run()
