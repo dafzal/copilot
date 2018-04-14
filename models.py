1ecdecbfbb64f53f8c367a0e89f9e134ebde66ed
Danial Afzal <dafzal@lyft.com>
2018-04-14 00:52:03 -0700

    Update

--- models.py
+++ models.py
@@ -8,8 +8,7 @@ class User(Document):
 
     def to_json(self):
         return {
-            'user_id': self.user_id
-            ''
+            'user_id': self.user_id,
         }
 
 class HeadPosition(EmbeddedDocument):
