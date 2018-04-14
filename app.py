97f7f4e0c70a10760320b70a2f25d98297b1ec0d
Danial Afzal <dafzal@lyft.com>
2018-04-13 19:39:28 -0700

    add ulu

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 #Python libraries that we need to import for our bot
 import random
 from flask import Flask, request
+import json
+
 app = Flask(__name__)
 
 #We will receive messages that Facebook sends our bot at this endpoint 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/ulu", methods=['GET', 'POST'])
 def receive_message():
-    return "YOOO LETS GOOOO"
+    print request.get_json()
+    return "YOOO LETS GOOOO2"
 
 if __name__ == "__main__":
     app.run()
